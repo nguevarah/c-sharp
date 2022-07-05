88d6b351ff3c84ecc80bafcc8a366f4fb78f4aa7
Nely Guevara <Cursosdecomputacion.net@gmail.com>
2022-07-05 17:55:29 -0600

    varios con c-sharp

--- ejercicios/holamundo/Program.cs
+++ ejercicios/holamundo/Program.cs
@@ -6,11 +6,62 @@ namespace holamundo
     {
         static void Main(string[] args)
         {
-            string nombre = "";
-            Console.WriteLine("Ingrese su nombre");
-            nombre = Console.ReadLine();
+            /* string nombre = "";
+            Console.WriteLine("Ingrese su nombre"); // cout
+            nombre = Console.ReadLine(); // cin
 
-            Console.WriteLine("Hola " +  nombre);
+            Console.WriteLine("Hola " +  nombre); // cout endl */
+
+
+
+           /*  int edad = 0;
+
+            Console.WriteLine("Ingrese su edad");
+            edad = int.Parse(Console.ReadLine());
+
+            if (edad > 18)
+            {
+                Console.WriteLine("Eres mayor de edad");
+            }
+            else
+            {
+               Console.WriteLine("Eres menor de edad"); 
+            } */
+
+
+            /* int contador = 0;
+            while (true)
+            {
+                Console.Write("*");
+                if (contador == 100)
+                {
+                    break;
+                }
+                contador++;
+            } */
+
+
+            /* for (int i = 0; i < 10; i++)
+            {
+                Console.Write("*");
+            } */
+
+
+            Console.Write("Ingrese una opcion ");
+            int opcion = int.Parse(Console.ReadLine());
+            switch (opcion)
+            {
+                case 1:
+                    Console.WriteLine("1");
+                    break;  
+                case 2:
+                    Console.WriteLine("2");
+                    break;  
+                default:
+                    Console.WriteLine("Opcion no valida");
+                    break;
+            }
+
         }
     }
 }
