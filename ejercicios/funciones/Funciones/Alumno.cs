41185c7eb3590ccfd82996dea37e9a4c40e13924
Nely Guevara <Cursosdecomputacion.net@gmail.com>
2022-07-13 20:37:22 -0600

    funciones de retorno

---
+++ ejercicios/funciones/Funciones/Alumno.cs
@@ -0,0 +1,21 @@
+public class Alumno
+{
+    public int Id { get; set; }
+
+    public string PrimerNombre { get; set; }
+
+    public string SegundoNombre { get; set; }
+
+    public Alumno(int id, string primerNombre, string segundoNombre)
+    {
+        Id = id;
+        PrimerNombre = primerNombre;
+        SegundoNombre = segundoNombre;
+
+    }
+
+    public string nombreCompleto()
+    {
+        return PrimerNombre + " " + SegundoNombre;
+    }
+}
---
+++ ejercicios/funciones/Funciones/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Funciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno a = new Alumno(1, "Juan", "Perez");
+
+            Console.WriteLine(a.nombreCompleto());
+        }
+    }
+}
